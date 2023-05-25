7dd5567f69fa7ee7447bd3143d79576430d86236
Asif Anam <anam1828@cseku.ac.bd>
2023-05-25 16:41:42 +0600

    day end 25-5-23

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,7 +12,14 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number =1;
   searchMode: boolean = false;
+
+  //new properties for pagination
+  thePageNumber: number =1;
+  thePageSize: number =10;
+  theTotalElements: number =0;
+
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -56,13 +63,31 @@ export class ProductListComponent implements OnInit {
       // not category id available ... default to category id 1
       this.currentCategoryId = 1;
     }
+    //check if we have a different category than previous
+    //Angular will reuse a component if it is currently being viewed
+
+
+    //if we have a different category if than previous then set thePageNumber = 1
+    if(this.previousCategoryId != this.currentCategoryId){
+      this.thePageNumber =1;
+    }
+    this.previousCategoryId = this.currentCategoryId
+    console.log(`currentCategoryId = ${this.currentCategoryId}, thePageNumber = ${this.thePageNumber}`)
 
     // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
+    this.productService.getProductListPaginate(this.thePageNumber-1, this.thePageSize ,this.currentCategoryId).subscribe(
         data => {
-          this.products = data;
+          this.products = data._embedded.products;
+          this.thePageNumber = data.page.number +1;
+          this.thePageSize = data.page.size;
+          this.theTotalElements = data.page.totalElements;
         }
     )
+  }
+  updatePageSize(pageSize: string){
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1
+    this.listProducts()
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -16,7 +16,14 @@ export class ProductService {
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
+    getProductListPaginate(thePage: number, thePageSize: number ,theCategoryId: number): Observable<GetResponseProducts> {
 
+        // need to build URL based on category id
+
+        const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}` + `&page=${thePage}&size=${thePageSize}`;
+
+        return this.httpClient.get<GetResponseProducts>(searchUrl);
+    }
   getProductList(theCategoryId: number): Observable<Product[]> {
 
     // need to build URL based on category id 
@@ -53,7 +60,14 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
-  }
+  },
+    page:{
+      size: number,
+        totalElements: number,
+        totalPages: number,
+        number: number
+
+    }
 }
 interface GetResponseProductCategory {
     _embedded: {
