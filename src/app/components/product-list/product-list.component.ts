a4a9346d672e800c740e5b418cd56b1c42b2c703
Asif Anam <anam1828@cseku.ac.bd>
2023-05-28 17:08:07 +0600

    day end 28-5-23

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -19,7 +19,7 @@ export class ProductListComponent implements OnInit {
   thePageNumber: number =1;
   thePageSize: number =10;
   theTotalElements: number =0;
-
+  previousKeyword: string = "";
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -44,12 +44,16 @@ export class ProductListComponent implements OnInit {
 
   handleSearchProducts(){
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    //if we have a different keyword than previous
+    //then set thePageNumber to 1
+    if(this.previousKeyword != theKeyword){
+      this.thePageNumber = 1;
+    }
+    this.previousKeyword = theKeyword;
+    console.log(`keyword = ${theKeyword}, thePageNumber = ${this.thePageNumber}`);
     //search for the product using keyword
-    this.productService.searchProducts(theKeyword).subscribe(
-        data => {
-          this.products = data;
-        }
-    );
+    this.productService.searchProductListPaginate(this.thePageNumber -1, this.thePageSize, theKeyword).subscribe(this.processResult());
   }
   handleListProducts(){
     // check if "id" parameter is available
@@ -76,18 +80,24 @@ export class ProductListComponent implements OnInit {
 
     // now get the products for the given category id
     this.productService.getProductListPaginate(this.thePageNumber-1, this.thePageSize ,this.currentCategoryId).subscribe(
-        data => {
-          this.products = data._embedded.products;
-          this.thePageNumber = data.page.number +1;
-          this.thePageSize = data.page.size;
-          this.theTotalElements = data.page.totalElements;
-        }
-    )
+        this.processResult());
   }
   updatePageSize(pageSize: string){
     this.thePageSize = +pageSize;
     this.thePageNumber = 1
     this.listProducts()
   }
+  processResult(){
+    return(data:any) =>{
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number +1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    }
+  }
+  addToCart(theProduct: Product){
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`)
 
+    //the real work
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -38,6 +38,14 @@ export class ProductService {
 
         return this.getProducts(searchUrl);
     }
+    searchProductListPaginate(thePage: number, thePageSize: number ,theKeyword: string): Observable<GetResponseProducts> {
+
+        // need to build URL based on keyword
+
+        const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}` + `&page=${thePage}&size=${thePageSize}`;
+
+        return this.httpClient.get<GetResponseProducts>(searchUrl);
+    }
 
     private getProducts(searchUrl: string) {
         return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
