13518201254deb13fce22e7450813514f4f47e58
Asif Anam <anam1828@cseku.ac.bd>
2023-06-11 16:36:26 +0600

    day end 11-6-23 video 180

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ShopFormService} from "../../services/shop-form.service";
 import {Country} from "../../common/country";
+import {State} from "../../common/state";
+import {
+  logExperimentalWarnings
+} from "@angular-devkit/build-angular/src/builders/browser-esbuild/experimental-warnings";
 
 @Component({
   selector: 'app-checkout',
@@ -17,6 +21,9 @@ export class CheckoutComponent implements OnInit {
   creditCardMonths: number[] = [];
   countries: Country[] = [];
 
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
   constructor(private formBuilder: FormBuilder,
               private ShopFormService: ShopFormService) { }
 
@@ -24,9 +31,10 @@ export class CheckoutComponent implements OnInit {
 
     this.checkoutFormGroup= this.formBuilder.group({
       customer:this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('',
+            [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2-4}$')])
       }),
       shippingAddress:this.formBuilder.group({
         street:[''],
@@ -77,20 +85,38 @@ export class CheckoutComponent implements OnInit {
         }
     )
 
+
   }
   onSubmit(){
     console.log("Handling the submit button");
+    if(this.checkoutFormGroup.invalid){
+      this.checkoutFormGroup.markAllAsTouched();
+    }
     console.log(this.checkoutFormGroup.get('customer')?.value)
-    console.log("the email address is " + this.checkoutFormGroup.get('customer')?.value.email)
+    console.log("the email address is " + this.checkoutFormGroup.get('customer')?.value.email);
+    console.log("the shipping address country is " +this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
+    console.log("the shipping address state is " +this.checkoutFormGroup.get('shippingAddress')?.value.state.name);
+
+
   }
+  get firstName(){return this.checkoutFormGroup.get('customer.firstName');}
+  get lastName(){return this.checkoutFormGroup.get('customer.lastName');}
+  get email(){return this.checkoutFormGroup.get('customer.email');}
+
 
   copyShippingAddressToBillingAddress(event: any) {
     if (event.target.checked) {
       this.checkoutFormGroup.controls['billingAddress']
           .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+
+      //BUG FIX FOR STATES
+      this.billingAddressStates = this.shippingAddressStates;
     }
     else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
+
+      //bug fix for states
+      this.billingAddressStates = [];
     }
   }
 
@@ -114,4 +140,30 @@ export class CheckoutComponent implements OnInit {
         }
     )
   }
+
+  getStates(formGroupName: string) {
+
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+    console.log(`${formGroupName} country code: ${countryCode}`)
+    console.log(`${formGroupName} country name: ${countryName}`)
+
+    this.ShopFormService.getStates(countryCode).subscribe(
+        data =>{
+          if(formGroupName === 'shippingAddress'){
+            this.shippingAddressStates = data;
+            console.log(data)
+          }
+          else {
+            this.billingAddressStates = data;
+          }
+          //select first state as default
+          formGroup?.get('state')!.setValue(data[0])
+        }
+    )
+  }
+
+  protected readonly logExperimentalWarnings = logExperimentalWarnings;
 }
