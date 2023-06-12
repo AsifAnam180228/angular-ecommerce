7b5b5f48d3ee7062066196f45a28a2cdbc979b58
Asif Anam <anam1828@cseku.ac.bd>
2023-06-12 16:39:31 +0600

    day end 12-6-23 video 196

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -6,6 +6,8 @@ import {State} from "../../common/state";
 import {
   logExperimentalWarnings
 } from "@angular-devkit/build-angular/src/builders/browser-esbuild/experimental-warnings";
+import {ShopValidators} from "../../validators/shop-validators";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-checkout',
@@ -25,38 +27,64 @@ export class CheckoutComponent implements OnInit {
   billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder,
-              private ShopFormService: ShopFormService) { }
+              private ShopFormService: ShopFormService,
+              private cartService: CartService) { }
 
   ngOnInit(): void {
 
+    this.reviewCartDetails();
+
     this.checkoutFormGroup= this.formBuilder.group({
       customer:this.formBuilder.group({
-        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
-        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        firstName: new FormControl('',
+            [Validators.required,
+              Validators.minLength(2),
+              ShopValidators.notOnlyWhitespace]),
+        lastName: new FormControl('',
+            [Validators.required,
+              Validators.minLength(2),
+            ShopValidators.notOnlyWhitespace]),
         email: new FormControl('',
             [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2-4}$')])
       }),
       shippingAddress:this.formBuilder.group({
-        street:[''],
-        city:[''],
-        state:[''],
-        country:[''],
-        zipCode:[''],
+        street:new FormControl('',
+            [Validators.required, Validators.minLength(2),ShopValidators.notOnlyWhitespace]),
+        city:new FormControl('',
+            [Validators.required, Validators.minLength(2),ShopValidators.notOnlyWhitespace]),
+        state:new FormControl('',
+            [Validators.required]),
+        country:new FormControl('',
+            [Validators.required]),
+        zipCode:new FormControl('',
+            [Validators.required, Validators.minLength(2),ShopValidators.notOnlyWhitespace]),
       }),
-      billingAddress:this.formBuilder.group({
-        street:[''],
-        city:[''],
-        state:[''],
-        country:[''],
-        zipCode:[''],
+
+      billingAddress: this.formBuilder.group({
+        street:new FormControl('',
+            [Validators.required, Validators.minLength(2),ShopValidators.notOnlyWhitespace]),
+        city:new FormControl('',
+            [Validators.required, Validators.minLength(2),ShopValidators.notOnlyWhitespace]),
+        state:new FormControl('',
+            [Validators.required]),
+        country:new FormControl('',
+            [Validators.required]),
+        zipCode:new FormControl('',
+            [Validators.required, Validators.minLength(2),ShopValidators.notOnlyWhitespace]),
       }),
       creditCard:this.formBuilder.group({
-        cardType:[''],
-        nameOnCard:[''],
-        cardNumber:[''],
-        securityCode:[''],
-        expirationMonth:[''],
-        expirationYear:[''],
+        cardType:new FormControl('',
+            [Validators.required]),
+        nameOnCard:new FormControl('',
+            [Validators.required, Validators.minLength(2),ShopValidators.notOnlyWhitespace]),
+        cardNumber:new FormControl('',
+            [Validators.required, Validators.pattern('[0-9]{16}')]),
+        securityCode:new FormControl('',
+            [Validators.required, Validators.pattern('[0-9]{3}')]),
+        expirationMonth:new FormControl('',
+            [Validators.required]),
+        expirationYear:new FormControl('',
+            [Validators.required]),
       })
         });
     //populate credit card months
@@ -102,6 +130,23 @@ export class CheckoutComponent implements OnInit {
   get firstName(){return this.checkoutFormGroup.get('customer.firstName');}
   get lastName(){return this.checkoutFormGroup.get('customer.lastName');}
   get email(){return this.checkoutFormGroup.get('customer.email');}
+  get shippingAddressStreet(){return this.checkoutFormGroup.get('shippingAddress.street');}
+  get shippingAddressCity(){return this.checkoutFormGroup.get('shippingAddress.city');}
+  get shippingAddressState(){return this.checkoutFormGroup.get('shippingAddress.state');}
+  get shippingAddressZipCode(){return this.checkoutFormGroup.get('shippingAddress.zipCode');}
+  get shippingAddressCountry(){return this.checkoutFormGroup.get('shippingAddress.country');}
+
+  get billingAddressStreet(){return this.checkoutFormGroup.get('billingAddress.street');}
+  get billingAddressCity(){return this.checkoutFormGroup.get('billingAddress.city');}
+  get billingAddressState(){return this.checkoutFormGroup.get('billingAddress.state');}
+  get billingAddressZipCode(){return this.checkoutFormGroup.get('billingAddress.zipCode');}
+  get billingAddressCountry(){return this.checkoutFormGroup.get('billingAddress.country');}
+
+  get creditCardType(){return this.checkoutFormGroup.get('creditCard.cardType');}
+  get creditCardNameOnCard(){return this.checkoutFormGroup.get('creditCard.nameOnCard');}
+  get creditCardNumber(){return this.checkoutFormGroup.get('creditCard.number');}
+  get creditCardSecurityCode(){return this.checkoutFormGroup.get('creditCard.securityCode');}
+
 
 
   copyShippingAddressToBillingAddress(event: any) {
@@ -166,4 +211,17 @@ export class CheckoutComponent implements OnInit {
   }
 
   protected readonly logExperimentalWarnings = logExperimentalWarnings;
+
+  reviewCartDetails() {
+
+    //subscribe to cartService.totalQuantity
+    this.cartService.totalQuantity.subscribe(
+        totalQuantity => this.totalQuantity = totalQuantity
+    );
+
+    //subscribe to cartService.totalPrice
+    this.cartService.totalPrice.subscribe(
+        totalPrice => this.totalPrice = totalPrice
+    );
+  }
 }
