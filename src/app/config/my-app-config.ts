fc3e554ce331e3e6630b2b25c80ec40073433ca7
Asif Anam <anam1828@cseku.ac.bd>
2023-06-13 17:18:30 +0600

    day end 13-6-23 video 227

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+    oidc:{
+        clientId: '0oa9xx9e86uBLLxCB5d7',
+        issuer: 'https://dev-99761550.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes:['openid', 'profile', 'email']
+
+    }
+
+}
